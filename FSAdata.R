a6930be429c9ff5543056fa1b936e0a5faf86b16
Kennedy Coleman <colemk16@uw.edu>
2017-12-04 21:52:47 -0800

    added comments to code

--- FSAdata.R
+++ FSAdata.R
@@ -1,6 +1,8 @@
 #setwd("/home/kennedy/Documents/UW/INFO\ 201/final-project")
 #FSA.data <- read_excel("PortfolioSummary.xls")
 
+# This function takes in the PortfolioSummary.xls file and outputs the data 
+# reorganized into a simple data frame
 getData <- function(file.xls) {
   FSA.data <- read_excel(file.xls)
   
@@ -10,25 +12,31 @@ getData <- function(file.xls) {
   library(stringr)
   library(tidyr)
   
+  # Creates list of the 4 types of loans
   loan.types <- unname(unlist(FSA.data[4,]))
   loan.types <- loan.types[!is.na(loan.types)]
   loan.types <- gsub(" Loans", "", loan.types)
   loan.types[2] <- "FFEL" 
   loan.types[4] <- "Total"
   
+  # Creates list of types of data looked at for each loan
   data.types <- unname(unlist(FSA.data[5,3:length(FSA.data)]))
   data.types <- gsub("in ", "", data.types)
   data.types <- gsub("\\s+", "", data.types)
   data.types[2] <- "Recipients(millions)"
   
+  # Combine loan types and data types into vector of column names
   finance.data <- drop_na(FSA.data[6:nrow(FSA.data), 3:length(FSA.data)])
   colnames(finance.data) <- paste0(rep(loan.types, each = 2), ".", data.types)
   
+  # Take subset of data to filter out names and NA values
+  # Add years column that duplicates any year previously seperated into quarters
   finance.data$federal.fiscal.year <- unname(unlist(FSA.data[6:30, 1]))
   years.subset <- finance.data$federal.fiscal.year[!is.na(finance.data$federal.fiscal.year)]
   years.subset <- years.subset[7:length(years.subset)]
   years.subset <- head(rep(years.subset, each = 4), -1)
   
+  # Apply column names to reorganized subset of data
   finance.data$federal.fiscal.year[7:nrow(finance.data)] <- years.subset
   
   return(finance.data)
