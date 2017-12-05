78724e5d5b2b0fd42286b58497cc200c916d57cf
Caden Mormon <mormonc@uw.edu>
2017-12-04 20:37:20 -0800

    Added shiny app

---
+++ StudentLoans/ui.R
@@ -0,0 +1,27 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Student Loan Data"),
+
+  sidebarLayout(
+    sidebarPanel(
+       
+    ),
+
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
